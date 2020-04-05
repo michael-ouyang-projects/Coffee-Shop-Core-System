c16e40c91ad84b938bc914c20e51c1f85f8b77ff
Michael Ouyang <michaelouyang19971997@gmail.com>
2020-04-05 12:07:14 +0800

    [Michael Ouyang] update kafka feature - send successfully

---
+++ src/main/java/com/ouyang/stream/kafka/KafkaConfiguration.java
@@ -0,0 +1,34 @@
+package com.ouyang.stream.kafka;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.kafka.clients.admin.AdminClientConfig;
+import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.core.KafkaAdmin;
+
+@Configuration
+public class KafkaConfiguration {
+
+	@Value(value = "${kafka.servers}")
+    private String kafkaServers;
+	
+	
+	@Bean
+	public KafkaAdmin admin() {
+	    Map<String, Object> configs = new HashMap<>();
+	    configs.put(AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG, kafkaServers);
+	    return new KafkaAdmin(configs);
+	}
+
+	@Bean
+	public NewTopic report() {
+		
+	    return new NewTopic("report", 1, (short) 1);
+	    
+	}
+	
+}
--- src/main/java/com/ouyang/stream/kafka/KafkaService.java
+++ src/main/java/com/ouyang/stream/kafka/KafkaService.java
@@ -1,7 +1,5 @@
 package com.ouyang.stream.kafka;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
@@ -13,29 +11,25 @@ import com.ouyang.transaction.object.Transaction;
 @Service
 public class KafkaService {
 
-	private static final Logger logger = LoggerFactory.getLogger(KafkaService.class);
-	
 	@Autowired
-    private KafkaTemplate<String, String> kafkaTemplate;
-	
+	private KafkaTemplate<String, String> kafkaTemplate;
+
 	@Autowired
 	private ObjectMapper objectMapper;
-	
-	
+
 	public void sendTransaction(Transaction transaction) {
-		
+
 		try {
-			
-			String  message = objectMapper.writeValueAsString(transaction);
-			logger.debug(String.format("#### -> Producing message -> %s", message));
-	        this.kafkaTemplate.send("report", message);
-			
+
+			String message = objectMapper.writeValueAsString(transaction);
+			this.kafkaTemplate.send("report", message);
+
 		} catch (JsonProcessingException e) {
-			
+
 			e.printStackTrace();
-			
+
 		}
-        
-    }
-	
+
+	}
+
 }
