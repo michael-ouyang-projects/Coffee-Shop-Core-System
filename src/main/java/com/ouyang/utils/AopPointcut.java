0a343346d911e92fee6ea34071c74702b78ae296
Michael Ouyang <michaelouyang19971997@gmail.com>
2020-05-04 15:19:03 +0800

    [Michael Ouyang] update error handling

---
+++ src/main/java/com/ouyang/utils/AopPointcut.java
@@ -0,0 +1,45 @@
+package com.ouyang.utils;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+public class AopPointcut {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(AopPointcut.class);
+
+    @Pointcut("within(com.ouyang.mvc.controller.*) && @within(org.springframework.stereotype.Controller)")
+    public void controllerLayer() {
+    }
+
+    @Pointcut("execution(public String *(..))")
+    public void publicMethod() {
+    }
+
+    @Pointcut("controllerLayer() && publicMethod()")
+    public void controllerPublicMethod() {
+    }
+
+    @Around("controllerPublicMethod()")
+    public String interceptRequest(ProceedingJoinPoint joinPoint) {
+
+        try {
+
+            return (String) joinPoint.proceed();
+
+        } catch (Throwable e) {
+
+            LOGGER.info("{}", e.getMessage());
+            return "error.html";
+
+        }
+
+    }
+
+}
