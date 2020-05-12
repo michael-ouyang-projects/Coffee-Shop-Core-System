432349a3f77362eac46e94ebbfdcfcf64d336b2d
Michael Ouyang <michaelouyang19971997@gmail.com>
2020-05-12 09:35:54 +0800

    Update TransactionService.java

--- src/main/java/com/ouyang/transaction/TransactionService.java
+++ src/main/java/com/ouyang/transaction/TransactionService.java
@@ -4,6 +4,7 @@ import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+import java.util.NoSuchElementException;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -17,92 +18,99 @@ import com.ouyang.mvc.model.TradeResponse;
 
 @Service
 public class TransactionService {
-	
-	@Autowired
-	private TransactionRepository trasactionRepository;
 
-	@Autowired
-	private CoffeeRepository goodsRepository;
-	
-	
-	@Transactional(rollbackFor = Exception.class)
-	public TradeResponse trade(TradeRequest tradeRequest) {
+    @Autowired
+    private TransactionRepository trasactionRepository;
 
-		Transaction transaction = this.saveTransactionAndBuyingGoods(tradeRequest);
-		return this.createTradeResponse(transaction);
+    @Autowired
+    private CoffeeRepository goodsRepository;
 
-	}
+    @Transactional(rollbackFor = Exception.class)
+    public TradeResponse trade(TradeRequest tradeRequest) {
 
-	private Transaction saveTransactionAndBuyingGoods(TradeRequest tradeRequest) {
+        Transaction transaction = this.saveTransactionAndBuyingGoods(tradeRequest);
+        return this.createTradeResponse(transaction);
 
-		Transaction transaction = new Transaction();
-		transaction.setTradeDate(new Date());
-		transaction.setCustomerId(tradeRequest.getCustomerId());
-		transaction.setBranchId(tradeRequest.getBranchId());
-		transaction.setTotalPrice(tradeRequest.getTotalPrice());
-		transaction.setItems(this.createTrasactionItems(transaction, tradeRequest.getBuyingList()));
-		return trasactionRepository.save(transaction);
+    }
 
-	}
+    private Transaction saveTransactionAndBuyingGoods(TradeRequest tradeRequest) {
 
-	private List<TransactionItem> createTrasactionItems(Transaction transaction, List<TradeCoffee> buyingList) {
+        Transaction transaction = new Transaction();
+        transaction.setTradeDate(new Date());
+        transaction.setCustomerId(tradeRequest.getCustomerId());
+        transaction.setBranchId(tradeRequest.getBranchId());
+        transaction.setTotalPrice(tradeRequest.getTotalPrice());
+        transaction.setItems(this.createTrasactionItems(transaction, tradeRequest.getBuyingList()));
+        return trasactionRepository.save(transaction);
 
-		List<TransactionItem> trasactionItems = new ArrayList<>();
+    }
 
-		for (TradeCoffee buyingGoods : buyingList) {
+    private List<TransactionItem> createTrasactionItems(Transaction transaction, List<TradeCoffee> buyingList) {
 
-			TransactionItem transactionItem = new TransactionItem();
-			transactionItem.setTransaction(transaction);
-			transactionItem.setCoffeeId(buyingGoods.getCoffeeId());
-			transactionItem.setAmount(buyingGoods.getAmount());
+        List<TransactionItem> trasactionItems = new ArrayList<>();
 
-			trasactionItems.add(transactionItem);
+        for (TradeCoffee buyingGoods : buyingList) {
 
-		}
+            TransactionItem transactionItem = new TransactionItem();
+            transactionItem.setTransaction(transaction);
+            transactionItem.setCoffeeId(buyingGoods.getCoffeeId());
+            transactionItem.setAmount(buyingGoods.getAmount());
 
-		return trasactionItems;
+            trasactionItems.add(transactionItem);
 
-	}
+        }
 
-	private TradeResponse createTradeResponse(Transaction transaction) {
+        return trasactionItems;
 
-		TradeResponse tradeResponse = new TradeResponse();
-		tradeResponse.setTradeId(transaction.getId());
-		tradeResponse.setTradeDate(transaction.getTradeDate());
-		tradeResponse.setTotalPrice(transaction.getTotalPrice());
-		tradeResponse.setBuyingList(this.createBuyingGoodsListAfterSave(transaction.getItems()));
+    }
 
-		return tradeResponse;
+    private TradeResponse createTradeResponse(Transaction transaction) {
 
-	}
+        TradeResponse tradeResponse = new TradeResponse();
+        tradeResponse.setTradeId(transaction.getId());
+        tradeResponse.setTradeDate(transaction.getTradeDate());
+        tradeResponse.setTotalPrice(transaction.getTotalPrice());
+        tradeResponse.setBuyingList(this.createBuyingGoodsListAfterSave(transaction.getItems()));
 
-	private List<TradeCoffee> createBuyingGoodsListAfterSave(List<TransactionItem> transactionItems) {
+        return tradeResponse;
 
-		List<TradeCoffee> buyingGoodsList = new ArrayList<>();
+    }
 
-		for (TransactionItem transactionItem : transactionItems) {
+    private List<TradeCoffee> createBuyingGoodsListAfterSave(List<TransactionItem> transactionItems) {
 
-			Coffee goods = goodsRepository.findById(transactionItem.getCoffeeId()).get();
-			
-			TradeCoffee buyingGoods = new TradeCoffee();
-			buyingGoods.setCoffeeId(transactionItem.getCoffeeId());
-			buyingGoods.setCoffeeName(goods.getName());
-			buyingGoods.setAmount(transactionItem.getAmount());
-			buyingGoods.setPrice(goods.getPrice());
-			buyingGoods.setSubtotal(goods.getPrice().multiply(new BigDecimal(transactionItem.getAmount())));
+        List<TradeCoffee> buyingGoodsList = new ArrayList<>();
 
-			buyingGoodsList.add(buyingGoods);
+        for (TransactionItem transactionItem : transactionItems) {
 
-		}
+            Coffee goods = goodsRepository.findById(transactionItem.getCoffeeId()).get();
 
-		return buyingGoodsList;
+            TradeCoffee buyingGoods = new TradeCoffee();
+            buyingGoods.setCoffeeId(transactionItem.getCoffeeId());
+            buyingGoods.setCoffeeName(goods.getName());
+            buyingGoods.setAmount(transactionItem.getAmount());
+            buyingGoods.setPrice(goods.getPrice());
+            buyingGoods.setSubtotal(goods.getPrice().multiply(new BigDecimal(transactionItem.getAmount())));
 
-	}
+            buyingGoodsList.add(buyingGoods);
 
-	public Transaction queryTrade(Long tradeId) {
+        }
 
-		return trasactionRepository.findById(tradeId).get();
+        return buyingGoodsList;
 
-	}
+    }
+
+    public Transaction queryTrade(Long tradeId) {
+
+        try {
+
+            return trasactionRepository.findById(tradeId).get();
+
+        } catch (NoSuchElementException e) {
+
+            throw new NoSuchElementException("Trade not exist!");
+
+        }
+
+    }
 
 }
