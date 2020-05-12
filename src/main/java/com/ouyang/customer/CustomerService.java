cefc131b814d5afe25694bf6e0b6114368de173c
Michael Ouyang <michaelouyang19971997@gmail.com>
2020-05-12 09:34:37 +0800

    Update CustomerService.java

--- src/main/java/com/ouyang/customer/CustomerService.java
+++ src/main/java/com/ouyang/customer/CustomerService.java
@@ -1,18 +1,28 @@
 package com.ouyang.customer;
 
+import java.util.NoSuchElementException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class CustomerService {
 
-	@Autowired
-	private CustomerRepository customerRepository;
-	
-	public Customer queryCustomerById(Long id) {
-		
-		return customerRepository.findById(id).get();
-		
-	}
-	
+    @Autowired
+    private CustomerRepository customerRepository;
+
+    public Customer queryCustomerById(Long id) {
+
+        try {
+
+            return customerRepository.findById(id).get();
+
+        } catch (NoSuchElementException e) {
+
+            throw new NoSuchElementException("Customer not exist!");
+
+        }
+
+    }
+
 }
