e3cca02fe7a43d5313ec69145f68c3fcbe47c02d
Michael Ouyang <michaelouyang19971997@gmail.com>
2020-05-12 09:34:13 +0800

    Update CoffeeService.java

--- src/main/java/com/ouyang/coffee/CoffeeService.java
+++ src/main/java/com/ouyang/coffee/CoffeeService.java
@@ -1,18 +1,28 @@
 package com.ouyang.coffee;
 
+import java.util.NoSuchElementException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class CoffeeService {
 
-	@Autowired
-	private CoffeeRepository coffeeRepository;
-	
-	public Coffee queryCoffeeById(Long id) {
-		
-		return coffeeRepository.findById(id).get();
-		
-	}
-	
+    @Autowired
+    private CoffeeRepository coffeeRepository;
+
+    public Coffee queryCoffeeById(Long id) {
+
+        try {
+
+            return coffeeRepository.findById(id).get();
+
+        } catch (NoSuchElementException e) {
+
+            throw new NoSuchElementException("No such Coffee!");
+
+        }
+
+    }
+
 }
