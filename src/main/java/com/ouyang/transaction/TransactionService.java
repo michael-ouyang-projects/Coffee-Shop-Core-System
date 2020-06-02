5e5657f74b9c44a754c8fcb6b91b4e7edd8074c8
Michael Ouyang <michaelouyang19971997@gmail.com>
2020-06-02 08:45:07 +0800

    Update TransactionService.java

--- src/main/java/com/ouyang/transaction/TransactionService.java
+++ src/main/java/com/ouyang/transaction/TransactionService.java
@@ -6,6 +6,8 @@ import java.util.Date;
 import java.util.List;
 import java.util.NoSuchElementException;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -28,19 +30,19 @@ public class TransactionService {
     @Transactional(rollbackFor = Exception.class)
     public TradeResponse trade(TradeRequest tradeRequest) {
 
-        Transaction transaction = this.saveTransactionAndBuyingGoods(tradeRequest);
+        Transaction transaction = this.saveTransactionAndTradeGoods(tradeRequest);
         return this.createTradeResponse(transaction);
 
     }
 
-    private Transaction saveTransactionAndBuyingGoods(TradeRequest tradeRequest) {
+    private Transaction saveTransactionAndTradeGoods(TradeRequest tradeRequest) {
 
         Transaction transaction = new Transaction();
         transaction.setTradeDate(new Date());
         transaction.setCustomerId(tradeRequest.getCustomerId());
         transaction.setBranchId(tradeRequest.getBranchId());
         transaction.setTotalPrice(tradeRequest.getTotalPrice());
-        transaction.setItems(this.createTrasactionItems(transaction, tradeRequest.getBuyingList()));
+        transaction.setItems(this.createTrasactionItems(transaction, tradeRequest.getTradeList()));
         return trasactionRepository.save(transaction);
 
     }
@@ -70,26 +72,32 @@ public class TransactionService {
         tradeResponse.setTradeId(transaction.getId());
         tradeResponse.setTradeDate(transaction.getTradeDate());
         tradeResponse.setTotalPrice(transaction.getTotalPrice());
-        tradeResponse.setBuyingList(this.createBuyingGoodsListAfterSave(transaction.getItems()));
+        tradeResponse.setTradeList(this.createTradeListAfterSave(transaction.getItems()));
 
         return tradeResponse;
 
     }
 
-    private List<TradeCoffee> createBuyingGoodsListAfterSave(List<TransactionItem> transactionItems) {
+    private List<TradeCoffee> createTradeListAfterSave(List<TransactionItem> transactionItems) {
 
         List<TradeCoffee> buyingGoodsList = new ArrayList<>();
 
         for (TransactionItem transactionItem : transactionItems) {
 
-            Coffee goods = goodsRepository.findById(transactionItem.getCoffeeId()).get();
+            if (transactionItem.getAmount() <= 0) {
+
+                continue;
+
+            }
+
+            Coffee coffee = goodsRepository.findById(transactionItem.getCoffeeId()).get();
 
             TradeCoffee buyingGoods = new TradeCoffee();
             buyingGoods.setCoffeeId(transactionItem.getCoffeeId());
-            buyingGoods.setCoffeeName(goods.getName());
+            buyingGoods.setCoffeeName(coffee.getName());
             buyingGoods.setAmount(transactionItem.getAmount());
-            buyingGoods.setPrice(goods.getPrice());
-            buyingGoods.setSubtotal(goods.getPrice().multiply(new BigDecimal(transactionItem.getAmount())));
+            buyingGoods.setPrice(coffee.getPrice());
+            buyingGoods.setSubtotal(coffee.getPrice().multiply(new BigDecimal(transactionItem.getAmount())));
 
             buyingGoodsList.add(buyingGoods);
 
@@ -110,6 +118,28 @@ public class TransactionService {
             throw new NoSuchElementException("Trade not exist!");
 
         }
+
+    }
+
+    @SuppressWarnings("unchecked")
+    @Transactional(rollbackFor = Exception.class)
+    public TradeResponse doReturn(HttpSession session) {
+
+        BigDecimal totalReturningPrice = (BigDecimal) session.getAttribute("totalReturningPrice");
+        List<TradeCoffee> returningList = (List<TradeCoffee>) session.getAttribute("returningList");
+
+        Transaction transaction = (Transaction) session.getAttribute("transaction");
+        transaction.setTotalPrice(transaction.getTotalPrice().subtract(totalReturningPrice));
+        List<TransactionItem> items = transaction.getItems();
+        returningList.forEach(returnsItem -> {
+
+            TransactionItem item = items.stream().filter(tmpItem -> tmpItem.getCoffeeId().equals(returnsItem.getCoffeeId())).findFirst().get();
+            item.setAmount(item.getAmount() - returnsItem.getAmount());
+
+        });
+
+        transaction = trasactionRepository.save(transaction);
+        return this.createTradeResponse(transaction);
 
     }
 
